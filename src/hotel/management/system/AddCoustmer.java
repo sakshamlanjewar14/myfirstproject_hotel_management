403bfec2a40626edfaa1442f5dba258acd5c1fc0
mgajapure6 <mgajapure6@gmail.com>
2025-08-31 12:52:56 +0530

    Added sql file and updated .java file

--- src/hotel/management/system/AddCoustmer.java
+++ src/hotel/management/system/AddCoustmer.java
@@ -4,18 +4,21 @@ import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import java.sql.ResultSet;
-import java.util.Date;
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 
-public class AddCoustmer extends JFrame {
+public class AddCoustmer extends JFrame implements ActionListener {
 
     JComboBox comboid;
     JTextField fno, fname, fcountry, fdeposit;
@@ -112,18 +115,18 @@ public class AddCoustmer extends JFrame {
             e.printStackTrace();
         }
         add(croom);
-        
+
         JLabel checkin = new JLabel("CHECKIN TIME");
         checkin.setBounds(35, 320, 150, 25);
         checkin.setFont(new Font("Raleway", Font.PLAIN, 16));
         add(checkin);
-        
+
         Date date = new Date();//getting screen window time
-        JLabel checkintime = new JLabel(""+ date);
+        JLabel checkintime = new JLabel("" + date);
         checkintime.setBounds(200, 320, 150, 25);
         checkintime.setFont(new Font("Raleway", Font.PLAIN, 15));
         add(checkintime);
-        
+
         JLabel deposit = new JLabel("DEPOSIT");
         deposit.setBounds(35, 360, 150, 25);
         deposit.setFont(new Font("Raleway", Font.PLAIN, 16));
@@ -132,22 +135,22 @@ public class AddCoustmer extends JFrame {
         fdeposit = new JTextField();
         fdeposit.setBounds(200, 360, 150, 25);
         add(fdeposit);
-        
-         //For add button
+
+        //For add button
         add = new JButton("ADD");
         add.setBounds(35, 400, 150, 30);
         add.setBackground(Color.BLACK);
         add.setForeground(Color.WHITE);
-       // add.addActionListener(this);
+        add.addActionListener(this);
         add(add);
-        
-         back = new JButton("BACK");
+
+        back = new JButton("BACK");
         back.setBounds(200, 400, 150, 30);
         back.setBackground(Color.BLACK);
         back.setForeground(Color.WHITE);
-       // add.addActionListener(this);
+        back.addActionListener(this);
         add(back);
-        
+
         //for image
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/fifth.png"));
         Image i2 = i1.getImage().getScaledInstance(300, 400, Image.SCALE_DEFAULT);    //for scaling image
@@ -155,9 +158,6 @@ public class AddCoustmer extends JFrame {
         JLabel image = new JLabel(i3);
         image.setBounds(400, 50, 300, 400);
         add(image);
-
-        
-        
 
         setTitle("AddCoustmer");
         getContentPane().setBackground(Color.WHITE);
@@ -169,6 +169,32 @@ public class AddCoustmer extends JFrame {
 
     public static void main(String[] args) {
         new AddCoustmer();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == add) {
+
+            String id = (String) comboid.getSelectedItem();
+            String number = fno.getText();
+            String name = fname.getText();
+            String gender = null;
+
+            if (rbmale.isSelected()) {
+                gender = "MALE";
+            } else if (rbfemale.isSelected()) {
+                gender = "FEMALE";
+            }
+
+            String country = fcountry.getText();
+            String room = croom.getSelectedItem();
+            String time = checkintime.getText();
+             String deposit = fdeposit.getText();
+
+        } else if(e.getSource()== back){
+            
+        }
     }
 
 }
